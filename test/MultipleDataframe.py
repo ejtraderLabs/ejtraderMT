2f73ae55098a2b2d850226e028f4ad1367987435
Emerson Pedroso <emerson@ejtrader.com>
2021-02-24 20:46:04 -0300

    fix

---
+++ test/MultipleDataframe.py
@@ -0,0 +1,17 @@
+from ejtraderMT import Metatrader
+
+
+api = Metatrader()
+
+symbol = "EURUSD"
+symbols = [symbol,"GBPUSD","AUDUSD"]
+timeframe = "M1"
+fromDate = "01/01/2021"
+toDate = "01/02/2021"
+
+data = api.historyDataFrame(symbol,symbols,timeframe,fromDate,toDate)
+
+api.
+
+print(data)
+
