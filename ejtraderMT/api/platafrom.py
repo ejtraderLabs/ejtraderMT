02e53d84fa27a50c91c65db5d2dda0af166f9887
Emerson Pedroso <emerson@ejtrader.com>
2021-03-01 11:53:09 -0300

    clean

--- ejtraderMT/api/platafrom.py
+++ ejtraderMT/api/platafrom.py
@@ -367,18 +367,7 @@ class Metatrader:
         result = broker - delta
         print(result)
         return result
-
-    
-
-    # convert timestamp to hour minitus secods
-    def convertTimeStamp(self, seconds):
-        seconds = seconds % (24 * 3600)
-        hour = seconds // 3600
-        seconds %= 3600
-        minutes = seconds // 60
-        seconds %= 60
-
-        return "%d:%02d:%02d" % (hour, minutes, seconds)
+  
 
     def live(self, symbol, chartTF):
         self.api.Command(action="RESET")
