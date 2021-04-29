85286da4e0aaec7e17c595a538e738f3ebb99ea9
Emerson Pedroso <emerson@ejtrader.com>
2021-04-29 15:07:08 -0300

    upgrade loop for no db

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def requirements(filename):
 
 setup(
     name='ejtraderMT',
-    version='3.0.3',
+    version='3.0.4',
     packages=find_packages(),
     url='https://ejtraderMT.readthedocs.io/',
     download_url='https://ejtrader.com',
