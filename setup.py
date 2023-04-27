52f1303c94a6eec9c6eb1717282a717764a031dd
Emerson Pedroso <emerson@bitcoinnano.org>
2023-04-27 16:07:23 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def requirements(filename):
 
 setup(
     name='ejtraderMT',
-    version='3.11',
+    version='3.12',
     packages=find_packages(),
     url='https://ejtraderMT.readthedocs.io/',
     download_url='https://ejtrader.com',
